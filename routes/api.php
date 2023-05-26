50e434c52d0a7de87b34d6987c5cb3e9047aa636
dhawlesudhir <34824916+dhawlesudhir@users.noreply.github.com>
2023-05-26 20:51:49 +0530

    authentication added

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/test', function () {
+    return Auth::user();
+})->middleware('auth:sanctum');
